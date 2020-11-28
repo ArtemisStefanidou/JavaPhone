5cc87fd63ccd0dd86fdf09ec7221e749f50b70bc
Unknown <unknown@example.com>
2020-11-28 17:48:17 +0200

    Added basic fields

---
+++ it21996/src/main/java/org/hua/it21996/Contract.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.hua.it21996;
+
+/**
+ *
+ * @author artemis
+ */
+public class Contract {
+    
+    private int contractCode;
+    private int number;
+    private int vatNumber;
+    private int customerNumber;
+    private int startContract;
+    private int durationContract;
+    
+}
